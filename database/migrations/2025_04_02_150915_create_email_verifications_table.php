1a0aaeeab7a58da560dc3832d069d581e151e645
McGillll <mcgilibag123@gmail.com>
2025-04-08 12:41:42 +0800

    migration

--- database/migrations/2025_04_02_150915_create_email_verifications_table.php
+++ database/migrations/2025_04_02_150915_create_email_verifications_table.php
@@ -11,14 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('failed_jobs', function (Blueprint $table) {
+        Schema::create('email_verifications', function (Blueprint $table) {
             $table->id();
-            $table->string('uuid')->unique();
-            $table->text('connection');
-            $table->text('queue');
-            $table->longText('payload');
-            $table->longText('exception');
-            $table->timestamp('failed_at')->useCurrent();
+            $table->string('email')->unique();
+            $table->string('token');
+            $table->timestamps();
         });
     }
 
@@ -27,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('failed_jobs');
+        Schema::dropIfExists('email_verifications');
     }
 };
